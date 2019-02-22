1f07cd8fee62ebc8bc5290c59c2db2110d9922fd
ryanpig <ryanpig@gmail.com>
2019-02-22 09:16:49 +0100

    R: be adaptive to n player by modifying average payoff algorithm

--- RFiles/statistical_analysis.R
+++ RFiles/statistical_analysis.R
@@ -12,27 +12,32 @@ lDataFrames <- vector("list", length=length(tables))
 for (i in seq(along=tables)) {
   DF <- dbGetQuery(conn=con, statement=paste("SELECT * FROM '", tables[[i]], "'", sep=""))
 }
+## Average different round payoffs
+  total_iteration <- length(unique(DF$round))
+## create 0 value data
+  total <- subset(DF, DF$round == 0)[, "payoff_p0"]
+  total[] = 0
+## get average of payoffs
+  for(i in seq(total_iteration)){
+    tmp <- subset(DF, DF$round == i - 1)
+    total <- total + tmp[, paste("payoff_p", i-1,sep="")]
+  }
+  avg_DF <- total[] / total_iteration
+  new_DF <- tmp[, c("gametype", "type_p0", "type_p1")]
+  final <- cbind(new_DF,avg_DF)
 
-odd_index = seq(1, nrow(DF), 2)
-even_index = seq(2, nrow(DF), 2)
-tmp1 <- DF[odd_index, ]
-tmp2 <- DF[even_index, ]
-avg_DF <- (tmp1[, "payoff_p0"] + tmp2[, "payoff_p1"]) / 2
-new_DF <- tmp1[, c("type_p0", "type_p1")]
-final <- cbind(new_DF,avg_DF)
+## Drawing
+  #png("boxplot.png", 640,480)
+  boxplot(avg_DF ~ type_p0, data=final, las=2)
+  
+  #png("beanplot.png", 640, 480)
+  library("ggplot2")
+  ggplot(final, aes(x=type_p0, y=avg_DF))+geom_boxplot()+geom_violin(fill='lightblue', alpha=0.5)+geom_jitter(position = position_jitter(width = .1))
 
-png("boxplot.png", 640,480)
-boxplot(avg_DF ~ type_p0, data=final, las=2)
-
-png("beanplot.png", 640, 480)
-##bean plot
-library("ggplot2")
-ggplot(final, aes(x=type_p0, y=avg_DF))+geom_boxplot()+geom_violin(fill='lightblue', alpha=0.5)+geom_jitter(position = position_jitter(width = .1))
-
-##?
-library("mosaic")#favstats
-favstats(avg_DF ~ type_p0,data=final)
+## descriptive statistics
+  library("mosaic")#favstats
+  favstats(avg_DF ~ type_p0,data=final)
 
 ## finish 
-dbDisconnect(con)
-dev.off()
+  dbDisconnect(con)
+  #dev.off()
