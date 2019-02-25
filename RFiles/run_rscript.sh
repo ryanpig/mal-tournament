9ecbd56395f9b44d4c2cb1280317bcd841779208
ryanpig <ryanpig@gmail.com>
2019-02-25 15:26:28 +0100

    update images w/ 5900 instatnce (29 games * 10 * 10 stratetgy pairs)

--- RFiles/run_rscript.sh
+++ RFiles/run_rscript.sh
@@ -3,5 +3,5 @@ cmd1="Rscript statistical_analysis.R"
 cmd2="Rscript heatmap.R"
 cmd3="Rscript heatmap_algorithms.R"
 echo ${cmd1} | bash
-echo ${cmd2} | bash
-echo ${cmd3} | bash
+# echo ${cmd2} | bash
+# echo ${cmd3} | bash
